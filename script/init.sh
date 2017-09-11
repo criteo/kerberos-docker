f37ca36a458b3eb8ec225858d4496a435e194595
Gilles Legoux <g.legoux@criteo.com>
2017-09-11 18:41:07 +0200

    Remove scp to copy keytabs, use docker cp instead

--- script/init.sh
+++ script/init.sh
@@ -4,9 +4,11 @@
 #
 # Init docker containers for kerberos cluster.
 
+cd "$(dirname "$0")"
+cd ..
+
 echo "=== Init krb5-service docker container ==="
 docker exec krb5-service /bin/bash -c '
-echo -e "root\nroot" | passwd
 echo -e "bob\nbob" | adduser bob --gecos ""
 /usr/sbin/sshd -f /etc/ssh/sshd_config
 ps -e | grep sshd
@@ -30,18 +32,24 @@ bob
 bob
 addprinc -randkey host/krb5-service.example.com@EXAMPLE.COM
 ktadd -k /etc/krb5-service.keytab host/krb5-service.example.com@EXAMPLE.COM
+ktadd -k /etc/bob.keytab bob@EXAMPLE.COM
 listprincs
 quit
 EOF
-
-# Send keytab to kerberized service
-sshpass -p "root" \
-  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-  /etc/krb5-service.keytab root@krb5-service.example.com:/etc/krb5.keytab
 '
+
+echo "=== Copy keytabs to krb5-service and krb5-machine ==="
+mkdir -vp ./tmp/
+docker cp krb5-kdc-server:/etc/krb5-service.keytab ./tmp/krb5-service.keytab
+docker cp krb5-kdc-server:/etc/bob.keytab ./tmp/bob.keytab
+docker cp ./tmp/krb5-service.keytab krb5-service:/etc/krb5.keytab
+docker cp ./tmp/bob.keytab krb5-machine:/etc/bob.keytab
+
 
 echo "=== Init krb5-machine docker container ==="
 docker exec krb5-machine /bin/bash -c '
 echo "bob" | kinit bob
+kinit -kt /etc/bob.keytab bob@EXAMPLE.COM
 klist
 '
+
