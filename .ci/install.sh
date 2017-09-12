cfcd36f0bd61c9f9e4a1d11d915c1f2c9c3d9bd5
Gilles Legoux <g.legoux@criteo.com>
2017-09-13 01:18:36 +0200

    Add installation of virtual python environment for travis CI

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,9 +2,24 @@
 #
 # install.sh
 #
-# Install for Continous Integration (CI) travis-ci.org.
+# Install environment for Continous Integration (CI) travis-ci.org.
 
+cd "$(dirname "$0")"
+cd ..
+
+echo "=== Install python environment for generating configuration ==="
+sudo apt-get -y install python3-pip python3-dev
+pip3 install virtualenv
+python3 --version
+pip3 --version
+pip3 freeze
+script/build-python-env.sh
+
+echo "=== Install bats for bash unit test ==="
 sudo add-apt-repository ppa:duggan/bats --yes
 sudo apt-get update -qq
 sudo apt-get install -qq bats
+bats --version
+
+echo "=== Install Kerberos environment in docker containers ==="
 make install
