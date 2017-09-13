2bfea933a652671f1ba48a85a061a121a3759137
Gilles Legoux <g.legoux@criteo.com>
2017-09-13 14:11:17 +0200

    Improve cleaning to choose step by step

--- script/clean.sh
+++ script/clean.sh
@@ -10,21 +10,23 @@ cd ..
 read -p "Do you want to remove virtual python environment "\
 "(./env folder)? [Y/n]: " \
 answer
-if [ "${answer}" != "Y" ]; then
-  exit 0
+if [[ "${answer}" == "Y" ]]; then
+  rm -v -rf ./env
 fi
-rm -v -rf ./env
 
-read -p "Do you want to remove services "\
-"(docker containers, images and network)? [Y/n]: " \
+read -p "Do you want to remove services as docker containers? [Y/n]: " \
 answer
-if [ "${answer}" != "Y" ]; then
+if [[ "${answer}" != "Y" ]]; then
   exit 0
 fi
-
 echo "=== Remove services as docker container ==="
 docker-compose rm --force
 
+read -p "Do you want to remove services as docker images? [Y/n]: " \
+answer
+if [[ "${answer}" != "Y" ]]; then
+  exit 0
+fi
 echo "=== Remove services as docker image ==="
 image_ids=$(docker images --filter "reference=krb5-*" -q)
 if [[ -z "${image_ids}" ]]; then
@@ -33,6 +35,11 @@ else
   docker rmi ${image_ids}
 fi
 
+read -p "Do you want to remove services network? [Y/n]: " \
+answer
+if [[ "${answer}" != "Y" ]]; then
+  exit 0
+fi
 echo "=== Remove example.com docker network ==="
 network_id=$(docker network ls --filter "name=example.com" -q)
 if [[ -z "${network_id}" ]]; then
@@ -44,9 +51,8 @@ fi
 read -p "Do you want to remove generated configuration "\
 "(docker-compose.yml file)? [Y/n]: " \
 answer
-if [ "${answer}" != "Y" ]; then
-  exit 0
+if [[ "${answer}" == "Y" ]]; then
+  rm -v docker-compose.yml
 fi
-rm -v docker-compose.yml
 
 echo "You must remove your minimal-<os> image at the hand, if you want..."
