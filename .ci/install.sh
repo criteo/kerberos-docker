abd426832d48419aba6a885102b670a1842fed33
glegoux <g.legoux@criteo.com>
2019-01-29 22:54:33 +0100

    Upgrade ubuntu version from 14.04 to 16.04 for Travis CI

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,7 +3,7 @@
 # install.sh
 #
 # Install environment for Continous Integration (CI) travis-ci.org.
-# On Ubuntu 14.04.5 LTS (for the moment).
+# On Ubuntu 16.04 LTS (for the moment).
 
 set -e
 
@@ -13,7 +13,7 @@ cd ..
 sudo apt-get update
 
 echo "=== Install python environment for generating configuration ==="
-sudo apt-get -y install python3-pip python3-dev
+sudo apt -y install python3-pip python3-dev
 sudo pip3 install virtualenv
 python3 --version
 pip3 --version
@@ -32,10 +32,9 @@ echo "...OK"
 
 echo "=== Install java and maven ==="
 sudo add-apt-repository ppa:webupd8team/java -y
-sudo add-apt-repository ppa:andrei-pozolotin/maven3 -y
-sudo apt-get update
+sudo apt update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get -y install oracle-java8-installer
-sudo apt-get -y install oracle-java8-set-default
-sudo apt-get install maven3
+sudo apt -y install oracle-java8-installer
+sudo apt -y install oracle-java8-set-default
+sudo apt install maven
 echo "...OK"
